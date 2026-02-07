b07347c554e9ff528ab045e6cbfa66f35e266546
J-AI-71 <240563060+J-AI-71@users.noreply.github.com>
2026-02-07 12:00:57 +0100

    Delete js/page-quickstart.js

--- js/page-quickstart.js
+++
@@ -1,7 +0,0 @@
-/* /Supersystem/js/page-quickstart.js (v35) */
-(() => {
-  'use strict';
-  const btn = document.getElementById('do-print');
-  if (btn) btn.addEventListener('click', () => window.print());
-  console.log('[SafeShare] quickstart ready');
-})();
